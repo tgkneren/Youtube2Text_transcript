3b50cadfc52101a05d5ba879113cbaca23cac521
tgkneren <113031951+tgkneren@users.noreply.github.com>
2023-07-02 19:13:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+
+
+def download_transcript(video_id):
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    text = ""
+    for segment in transcript:
+        text += segment['text'] + ' '
+    return text
+
+
+def save_text_to_file(text, file_path):
+    try:
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(text)
+        print("Metin dosyası başarıyla kaydedildi.")
+    except Exception as e:
+        print("Metin dosyası kaydedilirken bir hata oluştu:", str(e))
+
+
+video_id = 'QXWNChoIluo'  # https://www.youtube.com/watch?v=QXWNChoIluo (youtube link id)
+transcript_text = download_transcript(video_id)
+save_text_to_file(transcript_text, 'transcript.txt')
